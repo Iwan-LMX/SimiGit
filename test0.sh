e41f5d7a28276366e9cc5ea8c27661b71a8e58a0
UNSW_Iwan <z5433288@ad.unsw.edu.au>
2024-08-01 18:38:00 +1000

    Half

--- test0.sh
+++ test0.sh
@@ -1,11 +1,29 @@
 #! /bin/dash
 # This is for test grip-rm and grip commit
-rm .grip
-grip-commit -m dfdf
+# My code temp
+{
+    rm .grip
+    ./grip-commit -m dfdf
+    touch a
+    ./grip-add :a
+    ./grip-rm --force a
+    ./grip-commit -m dffdfd
+} > temp 2>&1
 
-touch a
-grip-add :a
+# correct 
+{
+    rm .grip
+    2041 grip-commit -m dfdf
+    touch a
+    2041 grip-add :a
+    2041 grip-rm --force a
+    2041 grip-commit -m dffdfd
+} > temp2041 2>&1
 
-grip-rm --force a
+# Compare the files and output result
+if diff temp temp2041 > /dev/null 2>&1; then
+    echo "pass"
+else
+    diff temp temp2041
+fi
 
-grip-commit -m dffdfd
---
+++ test4.sh
@@ -0,0 +1,42 @@
+#! /bin/dash
+# This is for test grip-checkout
+# My code temp
+{
+    rm -rf .grip
+    ./grip-checkout
+    ./grip-init
+    touch a
+    ./grip-add :a
+    ./grip-rm --force a
+    ./grip-commit -m dffdfd
+} > temp 2>&1
+
+# correct 
+{
+    rm -rf .grip
+    2041 grip-checkout
+    2041 grip-init
+    2041 grip-checkout
+
+    touch a
+    2041 grip-add :a
+    2041 grip-commit -m "first commit"
+    2041 grip-checkout dfdfd
+    2041 grip-checkout "dfdfd d"
+    2041 grip-checkout trunck
+    #Test checkout and log
+    2041 grip-branch new
+    2041 grip-checkout new
+    touch b
+    2041 grip-add b
+    2041 grip-commit -m "second"
+    2041 grip-log
+} > temp2041 2>&1
+
+# Compare the files and output result
+if diff temp temp2041 > /dev/null 2>&1; then
+    echo "pass"
+else
+    diff temp temp2041
+fi
+
