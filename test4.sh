f45b61740547d30c1cf1f178461b662867a5d216
UNSW_Iwan <z5433288@ad.unsw.edu.au>
2024-08-02 13:10:56 +1000

    Backup

--- test4.sh
+++ test4.sh
@@ -18,19 +18,24 @@
     2041 grip-init
     2041 grip-checkout
 
-    touch a
-    2041 grip-add :a
+    echo "new line" > a
+    2041 grip-add a
     2041 grip-commit -m "first commit"
     2041 grip-checkout dfdfd
     2041 grip-checkout "dfdfd d"
-    2041 grip-checkout trunck
+    2041 grip-checkout trunk
     #Test checkout and log
     2041 grip-branch new
     2041 grip-checkout new
     touch b
     2041 grip-add b
+    2041 grip-checkout trunk
+    2041 grip-checkout new
+
     2041 grip-commit -m "second"
     2041 grip-log
+    #Test checkout index
+    2041 grip-show :b
 } > temp2041 2>&1
 
 # Compare the files and output result
